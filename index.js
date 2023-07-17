6bbf53253476154e92088dd1577a6fd2489a4ce3
FabianMrkwitz <fmrkwitz@gmail.com>
2023-07-17 10:42:33 +0200

    solved Task character card component

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+import { createCharacterCard } from "./components/card/card.js";
 const cardContainer = document.querySelector('[data-js="card-container"]');
 const searchBarContainer = document.querySelector(
   '[data-js="search-bar-container"]'
@@ -7,8 +8,11 @@ const navigation = document.querySelector('[data-js="navigation"]');
 const prevButton = document.querySelector('[data-js="button-prev"]');
 const nextButton = document.querySelector('[data-js="button-next"]');
 const pagination = document.querySelector('[data-js="pagination"]');
+export const cardList = document.querySelector('[data-js="card-list"]');
 
 // States
 const maxPage = 1;
 const page = 1;
 const searchQuery = "";
+
+createCharacterCard("Bild", "Fabian", "jack", "test", "test");
